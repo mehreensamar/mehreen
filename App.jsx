a82ea0cf7f4f7423ef3df00854e11af51a731293
Mehreen Samar <mehreensamar09@gmail.com>
2025-06-25 14:32:46 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center text-center p-10">
+      <h1 className="text-5xl font-bold text-blue-600">Mehreen Samar</h1>
+      <p className="text-lg text-gray-700 mt-2">AI/ML | Web Developer</p>
+    </div>
+  );
+}
